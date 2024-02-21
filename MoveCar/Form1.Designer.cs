21d126b3ddea88eea440993716d6496196ff37c4
Mohammad Mahdi Bigdeli <mohammadmahdi.bigdeli@yahoo.com>
2024-02-21 19:53:37 +0330

    this is first commit. I originally committed the first version of the project

---
+++ MoveCar/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace MoveCar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.keyEvent = new System.Windows.Forms.TextBox();
+            this.dilayCar = new System.Timers.Timer();
+            this.road = new System.Windows.Forms.PictureBox();
+            this.car = new System.Windows.Forms.PictureBox();
+            this.start = new System.Timers.Timer();
+            this.moveCar = new System.Timers.Timer();
+            this.barrier1 = new System.Windows.Forms.PictureBox();
+            this.barrier2 = new System.Windows.Forms.PictureBox();
+            this.barrier3 = new System.Windows.Forms.PictureBox();
+            this.barrier4 = new System.Windows.Forms.PictureBox();
+            this.message1 = new System.Windows.Forms.Label();
+            this.message2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dilayCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moveCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrier1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrier2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrier3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrier4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // keyEvent
+            // 
+            this.keyEvent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(91)))), ((int)(((byte)(108)))));
+            this.keyEvent.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.keyEvent.Location = new System.Drawing.Point(0, 605);
+            this.keyEvent.Name = "keyEvent";
+            this.keyEvent.Size = new System.Drawing.Size(59, 15);
+            this.keyEvent.TabIndex = 2;
+            this.keyEvent.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyEvent_KeyDown);
+            this.keyEvent.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyEvent_KeyUp);
+            // 
+            // dilayCar
+            // 
+            this.dilayCar.Enabled = true;
+            this.dilayCar.Interval = 1D;
+            this.dilayCar.SynchronizingObject = this;
+            this.dilayCar.Elapsed += new System.Timers.ElapsedEventHandler(this.dilayCar_Elapsed);
+            // 
+            // road
+            // 
+            this.road.Image = global::MoveCar.Properties.Resources.full_road;
+            this.road.Location = new System.Drawing.Point(0, -600);
+            this.road.Margin = new System.Windows.Forms.Padding(0);
+            this.road.Name = "road";
+            this.road.Size = new System.Drawing.Size(300, 1200);
+            this.road.TabIndex = 4;
+            this.road.TabStop = false;
+            // 
+            // car
+            // 
+            this.car.BackColor = System.Drawing.Color.Transparent;
+            this.car.Image = global::MoveCar.Properties.Resources.car;
+            this.car.Location = new System.Drawing.Point(105, 355);
+            this.car.Margin = new System.Windows.Forms.Padding(0);
+            this.car.Name = "car";
+            this.car.Size = new System.Drawing.Size(90, 174);
+            this.car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car.TabIndex = 5;
+            this.car.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.Interval = 1D;
+            this.start.SynchronizingObject = this;
+            this.start.Elapsed += new System.Timers.ElapsedEventHandler(this.start_Elapsed);
+            // 
+            // moveCar
+            // 
+            this.moveCar.Interval = 1D;
+            this.moveCar.SynchronizingObject = this;
+            this.moveCar.Elapsed += new System.Timers.ElapsedEventHandler(this.moveCar_Elapsed);
+            // 
+            // barrier1
+            // 
+            this.barrier1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(91)))), ((int)(((byte)(108)))));
+            this.barrier1.Image = global::MoveCar.Properties.Resources.barrier;
+            this.barrier1.Location = new System.Drawing.Point(165, -90);
+            this.barrier1.Margin = new System.Windows.Forms.Padding(0);
+            this.barrier1.Name = "barrier1";
+            this.barrier1.Size = new System.Drawing.Size(90, 90);
+            this.barrier1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.barrier1.TabIndex = 6;
+            this.barrier1.TabStop = false;
+            // 
+            // barrier2
+            // 
+            this.barrier2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(91)))), ((int)(((byte)(108)))));
+            this.barrier2.Image = global::MoveCar.Properties.Resources.rock;
+            this.barrier2.Location = new System.Drawing.Point(165, -90);
+            this.barrier2.Margin = new System.Windows.Forms.Padding(0);
+            this.barrier2.Name = "barrier2";
+            this.barrier2.Size = new System.Drawing.Size(90, 90);
+            this.barrier2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.barrier2.TabIndex = 7;
+            this.barrier2.TabStop = false;
+            // 
+            // barrier3
+            // 
+            this.barrier3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(91)))), ((int)(((byte)(108)))));
+            this.barrier3.Image = global::MoveCar.Properties.Resources.barrier;
+            this.barrier3.Location = new System.Drawing.Point(45, -90);
+            this.barrier3.Margin = new System.Windows.Forms.Padding(0);
+            this.barrier3.Name = "barrier3";
+            this.barrier3.Size = new System.Drawing.Size(90, 90);
+            this.barrier3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.barrier3.TabIndex = 8;
+            this.barrier3.TabStop = false;
+            // 
+            // barrier4
+            // 
+            this.barrier4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(91)))), ((int)(((byte)(108)))));
+            this.barrier4.Image = global::MoveCar.Properties.Resources.rock;
+            this.barrier4.Location = new System.Drawing.Point(45, -90);
+            this.barrier4.Margin = new System.Windows.Forms.Padding(0);
+            this.barrier4.Name = "barrier4";
+            this.barrier4.Size = new System.Drawing.Size(90, 90);
+            this.barrier4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.barrier4.TabIndex = 9;
+            this.barrier4.TabStop = false;
+            // 
+            // message1
+            // 
+            this.message1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.message1.Font = new System.Drawing.Font("B Mehr", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.message1.ForeColor = System.Drawing.Color.White;
+            this.message1.Location = new System.Drawing.Point(-1, 90);
+            this.message1.Name = "message1";
+            this.message1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.message1.Size = new System.Drawing.Size(302, 45);
+            this.message1.TabIndex = 10;
+            this.message1.Text = "برای شروع فاصله را بزنید.";
+            this.message1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // message2
+            // 
+            this.message2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.message2.Font = new System.Drawing.Font("B Mehr", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.message2.ForeColor = System.Drawing.Color.White;
+            this.message2.Location = new System.Drawing.Point(-1, 140);
+            this.message2.Name = "message2";
+            this.message2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.message2.Size = new System.Drawing.Size(302, 45);
+            this.message2.TabIndex = 11;
+            this.message2.Text = "برای شروع مجدد R را بزنید.";
+            this.message2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.message2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(300, 600);
+            this.Controls.Add(this.message2);
+            this.Controls.Add(this.message1);
+            this.Controls.Add(this.barrier4);
+            this.Controls.Add(this.barrier3);
+            this.Controls.Add(this.barrier2);
+            this.Controls.Add(this.barrier1);
+            this.Controls.Add(this.car);
+            this.Controls.Add(this.road);
+            this.Controls.Add(this.keyEvent);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(306, 635);
+            this.MinimumSize = new System.Drawing.Size(306, 635);
+            this.Name = "Form1";
+            this.Text = "!تصادف نکن";
+            ((System.ComponentModel.ISupportInitialize)(this.dilayCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moveCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrier1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrier2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrier3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrier4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label message2;
+
+        private System.Windows.Forms.Label message1;
+
+        private System.Windows.Forms.PictureBox barrier3;
+
+        private System.Windows.Forms.PictureBox barrier4;
+
+        private System.Windows.Forms.PictureBox barrier1;
+
+        private System.Windows.Forms.PictureBox barrier2;
+
+        private System.Timers.Timer moveCar;
+
+        private System.Timers.Timer start;
+
+        private System.Windows.Forms.PictureBox car;
+
+        private System.Windows.Forms.PictureBox road;
+
+        private System.Timers.Timer dilayCar;
+
+        private System.Windows.Forms.TextBox keyEvent;
+
+        #endregion
+    }
+}
---
+++ MoveCar/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace MoveCar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            road.Controls.Add(car);
+            car.Top = 955;
+        }
+
+        private int car_y = 355, car_top = 355, speed_car = 1, move_car = 4, next_barrier = 300, a;
+        private bool start_move = false, in_right = false, new_barriers = true;
+
+        private Random random = new Random();
+        private bool[] new_barriers_law = new bool[4] { true, true, true, true };
+        private int[,] barriers_speed = new int[2, 4] { { 0, 0, 0, 0 }, { -90, -90, -90, -90 } };
+        private int[] stop_barriers = { 0, 1, 2, 3 };
+
+        private void dilayCar_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            road.Top += speed_car;
+            car.Top = car_y + (road.Top * -1);
+            if (road.Top >= 0)
+            {
+                road.Top = -600;
+                car.Top = car_y + (road.Top * -1);
+            }
+
+            car_top = car.Top + road.Top;
+
+            barrier1.Top += barriers_speed[0, 0];
+            barrier2.Top += barriers_speed[0, 1];
+            barrier3.Top += barriers_speed[0, 2];
+            barrier4.Top += barriers_speed[0, 3];
+
+            barriers_speed[1, 0] = barrier1.Top;
+            barriers_speed[1, 1] = barrier2.Top;
+            barriers_speed[1, 2] = barrier3.Top;
+            barriers_speed[1, 3] = barrier4.Top;
+
+            if (start_move)
+            {
+                if (new_barriers)
+                {
+                    a = stop_barriers[random.Next(0, stop_barriers.Length)];
+                    barriers_speed[0, a] = speed_car;
+                    stop_barriers = stop_barriers.Where(val => val != a).ToArray();
+                    new_barriers = false;
+                    next_barrier = random.Next(180, 450);
+                }
+            }
+
+            if (car_top <= barrier1.Top + 90 && car_top + 170 >= barrier1.Top + 20)
+            {
+                if (car.Left + 84 > barrier1.Left)
+                {
+                    message1.Text = "شما باختید!";
+                    message1.Visible = true;
+                    message2.Visible = true;
+                    dilayCar.Enabled = false;
+                    move_car = 0;
+                }
+            }
+
+            if (barrier1.Top >= next_barrier && new_barriers_law[0])
+            {
+                new_barriers_law[0] = false;
+                new_barriers = true;
+            }
+
+            if (barrier1.Top >= 600)
+            {
+                barriers_speed[0, 0] = 0;
+                barrier1.Top = -90;
+                Array.Resize(ref stop_barriers, stop_barriers.Length + 1);
+                stop_barriers[stop_barriers.Length - 1] = 0;
+                new_barriers_law[0] = true;
+            }
+
+
+            if (car_top <= barrier2.Top + 90 && car_top + 170 >= barrier2.Top + 10)
+            {
+                if (car.Left + 84 > barrier2.Left)
+                {
+                    message1.Text = "شما باختید!";
+                    message1.Visible = true;
+                    message2.Visible = true;
+                    dilayCar.Enabled = false;
+                    move_car = 0;
+                }
+            }
+
+            if (barrier2.Top >= next_barrier && new_barriers_law[1])
+            {
+                new_barriers_law[1] = false;
+                new_barriers = true;
+            }
+
+            if (barrier2.Top >= 600)
+            {
+                barriers_speed[0, 1] = 0;
+                barrier2.Top = -90;
+                Array.Resize(ref stop_barriers, stop_barriers.Length + 1);
+                stop_barriers[stop_barriers.Length - 1] = 1;
+                new_barriers_law[1] = true;
+            }
+
+
+            if (car_top <= barrier3.Top + 90 && car_top + 170 >= barrier3.Top + 20)
+            {
+                if (car.Left + 16 < barrier3.Left + 90)
+                {
+                    message1.Text = "شما باختید!";
+                    message1.Visible = true;
+                    message2.Visible = true;
+                    dilayCar.Enabled = false;
+                    move_car = 0;
+                }
+            }
+
+            if (barrier3.Top >= next_barrier && new_barriers_law[2])
+            {
+                new_barriers_law[2] = false;
+                new_barriers = true;
+            }
+
+            if (barrier3.Top >= 600)
+            {
+                barriers_speed[0, 2] = 0;
+                barrier3.Top = -90;
+                Array.Resize(ref stop_barriers, stop_barriers.Length + 1);
+                stop_barriers[stop_barriers.Length - 1] = 2;
+                new_barriers_law[2] = true;
+            }
+
+
+            if (car_top <= barrier4.Top + 90 && car_top + 170 >= barrier4.Top + 30)
+            {
+                if (car.Left + 16 < barrier4.Left + 90)
+                {
+                    message1.Text = "شما باختید!";
+                    message1.Visible = true;
+                    message2.Visible = true;
+                    dilayCar.Enabled = false;
+                    move_car = 0;
+                }
+            }
+
+            if (barrier4.Top >= next_barrier && new_barriers_law[3])
+            {
+                new_barriers_law[3] = false;
+                new_barriers = true;
+            }
+
+            if (barrier4.Top >= 600)
+            {
+                barriers_speed[0, 3] = 0;
+                barrier4.Top = -90;
+                Array.Resize(ref stop_barriers, stop_barriers.Length + 1);
+                stop_barriers[stop_barriers.Length - 1] = 3;
+                new_barriers_law[3] = true;
+            }
+        }
+
+        private void start_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (car_y > 340 && !(start_move))
+                car_y -= 2;
+            else
+            {
+                start_move = true;
+                start.Interval = 10;
+                speed_car = 4;
+
+                if (car_y < 400)
+                    car_y += 1;
+                else
+                {
+                    start.Enabled = false;
+                    in_right = false;
+                }
+
+                if (car.Left < 165)
+                    car.Left += 1;
+            }
+        }
+
+        private void moveCar_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (!(start.Enabled))
+            {
+                if (in_right)
+                {
+                    if (car.Left > 45)
+                    {
+                        car.Left -= move_car;
+                    }
+                    else
+                    {
+                        moveCar.Enabled = false;
+                    }
+                }
+                else
+                {
+                    if (car.Left < 165)
+                    {
+                        car.Left += move_car;
+                    }
+                    else
+                    {
+                        moveCar.Enabled = false;
+                    }
+                }
+            }
+            else
+            {
+                moveCar.Enabled = false;
+            }
+        }
+
+        private void keyEvent_KeyDown(object sender, KeyEventArgs e)
+        {
+            keyEvent.Text = "";
+            if (e.KeyCode == Keys.Space)
+            {
+            }
+        }
+
+        private void keyEvent_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!(start_move))
+                {
+                    message1.Visible = false;
+                    start.Enabled = true;
+                }
+                else
+                {
+                    in_right = !in_right;
+                    moveCar.Enabled = true;
+                }
+            }
+
+            if (e.KeyCode == Keys.R && start_move)
+            {
+                message1.Text = "برای شروع فاصله را بزنید.";
+                message1.Visible = true;
+                message2.Visible = false;
+                car_y = 355;
+                road.Top = -600;
+                car.Top = car_y + (road.Top * -1);
+                car.Left = 105;
+                speed_car = 1;
+                move_car = 4;
+                in_right = false;
+                start_move = false;
+                moveCar.Enabled = false;
+                barrier1.Top = -90;
+                barrier2.Top = -90;
+                barrier3.Top = -90;
+                barrier4.Top = -90;
+                barriers_speed = new int[2, 4] { { 0, 0, 0, 0 }, { -90, -90, -90, -90 } };
+                stop_barriers = new int[] { 0, 1, 2, 3 };
+                new_barriers = true;
+                new_barriers_law = new bool[4] { true, true, true, true };
+                dilayCar.Enabled = true;
+            }
+        }
+    }
+}
